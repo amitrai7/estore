d6303a165a00135a847603d3ad7f976086e1e343
amitrai7 <raiamit743@gmail.com>
2020-09-24 19:24:05 +0530

    bug fixed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from . import secrets
+ 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u*wf4wam$0jtnxl#dzb^$#a#0j@dzj%2c833w!ar-u9sb$$#m-'
+SECRET_KEY = secret_key
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
